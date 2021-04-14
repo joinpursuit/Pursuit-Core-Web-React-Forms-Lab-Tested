8e62b16288d2d3ce58f386ede30c3bb4243e470e
Alexander <alexanderl96@yahoo.com>
2021-04-14 16:32:28 -0400

    working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state ={
+      number: ""
+    }
+  }
+
+  handleValueChange =(e)=>{
+    const{name} = e.target
+    const{value} = e.target
+    this.setState({
+      [name]: value + ","
+    })
+
+  }
+
+  handleSum =(e)=>{
+    let sum = 0
+    for(let i = 0; i < e.taget.value; i++){
+      sum = sum + sum[i]
+    }
+    console.log(sum.value)
+    this.setState({
+      number: sum
+    })
+
+  }
+
+  handleAvarege =(e)=>{
+    const avera = this.setState({number: e.target.value})
+      
+    console.log(avera)
+    let average = avera.length
+    console.log(average.value)
+     
+    this.setState({
+      number: average
+    })
+
+
+  }
+  handleMode =(e) =>{
+
+  }
+
+  handleSubmit =(e) =>{
+    
+
+  }
+  render() {
+    return (
+      <div>
+       
+          <h1>Enter each number in the array, separated by a ','</h1>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor='type'></label>
+            <input type='text' name='number' placeholder="type" onChange={this.handleValueChange} value={this.state.number}></input>
+            <select >
+              <option></option>
+              <option value='Sum' onChange={this.handleSum}>Sum</option>
+              <option value='Mode'>Mode</option>
+              <option value='Average'>Average</option>
+            </select>
+            <button onChange={this.handleSum}>Calculate</button>
+            <ul onChange={this.handleSum}>
+
+            </ul>
+          </form>
+       
+        
+        
+      </div>
+    )
+  }
 }
 
-export default App;
