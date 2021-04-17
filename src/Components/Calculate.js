dea91e5b767319acee1e7806c7263b5567cde7f1
Coreen Cooper <coreencooper@pursuit.org>
2021-04-17 15:25:39 -0400

    add handleSelectChange method

--- src/Components/Calculate.js
+++ src/Components/Calculate.js
@@ -1,19 +1,40 @@
 import React from "react";
 
 class Calculate extends React.Component {
-  state = { inputArr: 0 };
+  state = { input: "", inputArr: "", mathDefault: "sum" };
 
-  handleCalculate = (e) => {
+  handleInputCalculate = (e) => {
     e.preventDefault();
   };
 
+  handleInputChange = (e) => {
+    this.setState({ input: e.target.value})
+    // console.log(e.target.value);
+  };
+
+  handleSelectChange = (e) => {
+    this.setState({ mathDefault: e.target.value})
+  }
+
   render() {
-    return (
-      <form onSubmit={this.handleCalculate}>
-        <label>
-          <input />
-        </label>
-      </form>
+      const { inputArr, mathDefault } = this.state;
+      console.log(this.state)
+      return (
+      <>
+        <form onSubmit={this.handleCalculate}>
+          <label>
+            Enter each number in the array, separated by a ','
+            <input onChange={this.handleInputChange} value={inputArr} />
+          </label>
+          <select onChange={this.handleSelectChange} value={mathDefault}>
+            <option value="sum">Sum</option>
+            <option value="average">Average</option>
+            <option value="mode">Mode</option>
+          </select>
+          <button>Calculate</button>
+        </form>
+        <p></p>
+      </>
     );
   }
 }
