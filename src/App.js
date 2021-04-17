cfa3eea4a18752cd8dabc988fb66653397ee2048
Shoaib Dar <SDAR30@gmail.com>
2021-04-17 17:06:47 -0400

    bonus attempt

--- src/App.js
+++ src/App.js
@@ -2,74 +2,80 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
-  state = { input: [], answer:"", operation: "sum", errorMessage:"" }
+  state = { input: [], answer: "", sum: false,  average: false, mode: false, errorMessage: "" }
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { input, answer, operation, errorMessage } = this.state;
+    let str = ""
+    const { input, operation, sum, average, mode} = this.state;
     if (input.length === 0) {
-      this.setState({errorMessage: "Invalid input."})
+      this.setState({ errorMessage: "Invalid input.", answer: "" })
     } else if (!input.every(value => !isNaN(value))) {
-      this.setState({errorMessage: "Invalid input."})
+      this.setState({ errorMessage: "Invalid input." , answer: ""})
     } else {
-      debugger
-      if (operation === "sum") {
-        let sum = input.reduce((a, b) => a + Number(b), 0);
-        console.log(sum)
-        this.setState({answer: sum})
-      } else if (operation === "average") {
-        let average = input.reduce((a, b) => a + Number(b), 0) / input.length;
-        console.log(average)
-        this.setState({answer: average})
-      } else if (operation === "mode") {
-        let modeInteger = this.mode(input)
-        console.log(modeInteger)
-        this.setState({answer: modeInteger})
-      } 
+      this.setState({ errorMessage: "" })
       
-    }
-    // debugger;
+      if (sum) {
+        let total = input.reduce((a, b) => a + Number(b), 0);
+        str += total + "\n"
+        //this.setState({ answer: total })
+      } if (average) {
+        let avg = input.reduce((a, b) => a + Number(b), 0) / input.length;
+        str += avg + "\n"
+        //this.setState({ answer: avg })
+      } if (mode) {
+        let modeArr = this.mode(input).join(",")
+        str += modeArr + "\n"
+        //this.setState({ answer: modeArr })
+      }
+      this.setState({ answer: str })
 
+    }
   };
 
   handleChange = (e) => {
-    // debugger  
     let inputArr = e.target.value.split(",");
     this.setState({ input: inputArr });
     console.log(inputArr)
   };
 
   handleEquation = (e) => {
-    this.setState({operation: e.target.value})
+    if(e.target.selectedOptions[0]){
+      this.setState({[e.target.selectedOptions[0].value]: true })
+    }
+    if(e.target.selectedOptions[1]){
+      this.setState({[e.target.selectedOptions[1].value]: true })
+    }
+    if(e.target.selectedOptions[2]){
+      this.setState({[e.target.selectedOptions[2].value]: true })
+    }
+    //this.setState({ operation: e.target.value })
   }
 
   mode = (numbers) => {
-    // as result can be bimodal or multi-modal,
-    // the returned result is provided as an array
-    // mode of [3, 5, 4, 4, 1, 1, 2, 3] = [1, 3, 4]
-    var modes = [], count = [], i, number, maxIndex = 0;
- 
+    let modes = [], count = [], i, number, maxIndex = 0;
+
     for (i = 0; i < numbers.length; i += 1) {
-        number = numbers[i];
-        count[number] = (count[number] || 0) + 1;
-        if (count[number] > maxIndex) {
-            maxIndex = count[number];
-        }
-    }
- 
-    for (i in count)
-        if (count.hasOwnProperty(i)) {
-            if (count[i] === maxIndex) {
-                modes.push(Number(i));
-            }
-        }
- 
-    return modes;
+      number = numbers[i];
+      count[number] = (count[number] || 0) + 1; 
+      if (count[number] > maxIndex) {
+        maxIndex = count[number];
       }
+    }
+
+    for (i in count)
+      if (count.hasOwnProperty(i)) {
+        if (count[i] === maxIndex) {
+          modes.push(Number(i));
+        }
+      }
+
+    return modes;
+  }
 
 
   render() {
-    const { input, answer, operation, errorMessage } = this.state;
+    const { input, answer, errorMessage } = this.state;
     console.log(this.state);
 
     return (
@@ -83,8 +89,8 @@ class App extends React.Component {
             value={input}
           />
           <br></br>
-          <select onChange={this.handleEquation}>
-            <option  defaultValue disabled={true}></option>
+          <select onChange={this.handleEquation} multiple>
+            <option defaultValue disabled={true}></option>
             <option value="sum">sum</option>
             <option value="average">average</option>
             <option value="mode">mode</option>
@@ -93,8 +99,7 @@ class App extends React.Component {
           <button>Calculate</button>
           <br></br>
           <h2 name="answer" value={answer}>{answer}</h2>
-          {/* {input === "a,b,c" ? <h2>Invalid Input.</h2> : null } */}
-        <h2>{errorMessage}</h2>
+          <h2>{errorMessage}</h2>
         </form>
       </>
     );
