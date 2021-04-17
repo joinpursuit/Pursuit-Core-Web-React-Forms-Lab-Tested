0b474880657f7f7270da8c250adabdb29871d651
Joshua <joshuafmendez@gmail.com>
2021-04-17 17:01:30 -0400

    convert input to an array

--- src/Components/Calculate.js
+++ src/Components/Calculate.js
@@ -1,18 +1,55 @@
 import React from "react";
 
 class Calculate extends React.Component {
-  state = { input: "", inputArr: "", mathDefault: "sum" };
+  state = { input: "", inputArr: [], mathDefault: "sum" , sum: 0, average: 0, mode: 0};
   
   handleInputCalculate = (e) => {
     e.preventDefault();
-    console.log(this.state);
+
+    console.log(this.state)
+    // const {input,inputArr,mathDefault,sum} = this.state
+    // console.log(input);
+
+    /*
+    we take in input convert to array,
+    map out the array convert each element of the array to a number
+
+    we can sum
+    we can average
+    we can mode 
+    */
+
+    // this.setState({
+     
+    // });
+    // const numbers = inputArr.map((num) => {
+    //   return parseInt(num);
+    // })
+    // console.log(inputArr);
+    // console.log(numbers)
+
+    // if(mathDefault === 'sum'){
+    //   this.setState({sum: inputArr.reduce((a, b) => a + b, sum)});
+    //   // console.log(this.state.sum);
+    // } else if (mathDefault === 'average') {
+    //   console.log('average');
+    // } else if (mathDefault === 'mode') {
+    //   console.log('mode');
+    // }
   };
+
+  /**  */
 
   handleInputChange = (e) => {
     this.setState({
-      input: e.target.value,
-      inputArr: [...this.state.input]
+      input: e.target.value.split(','),
+      // inputArr: this.input.map((num) => {
+      //     return parseInt(num);
+      //   })
+      
     });
+    
+
     // console.log(e.target.value);
   };
 
@@ -27,7 +64,7 @@ class Calculate extends React.Component {
         <form onSubmit={this.handleInputCalculate}>
           <label>
             Enter each number in the array, separated by a ','
-43            <input onChange={this.handleInputChange} value={input} />
+            <input onChange={this.handleInputChange} value={input} />
           </label>
           <select onChange={this.handleSelectChange} value={mathDefault}>
             <option value="sum">Sum</option>
