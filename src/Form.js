c1d44ab90ab8c2d662a05e63b71426b3183bd2e5
Jordan Bobadilla <jordanboba08@gmail.com>
2021-04-17 18:00:30 -0400

    almost there

--- src/Form.js
+++ src/Form.js
@@ -1,30 +1,62 @@
 import React from "react";
 
 class Form extends React.Component {
-    state = {
-        input : "",
-        select : ""
-    }
-    handleChange = (e) => {
-        const {name, value } = e.target
-        this.setState({[name]: value })
-    }
+  state = {
+    input: "",
+    select: "",
+    result: "",
+  };
+  handleChange = (e) => {
+    console.log(e.target);
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
   handleSubmit = (e) => {
     e.preventDefault();
+    const { input, select } = this.state;
+    if ((!isNaN(input) && input.includes(",")) || input !== "" ) {
+      const arr = input.split(",");
+      let sum = 0;
+      if (select === "sum") {
+        arr.map((el) => (sum += Number(el)));
+        this.setState({ result: sum });
+        this.setState({ input: "" });
+      } else if (select === "average") {
+        arr.map((el) => (sum += Number(el)));
+        const average = sum / arr.length;
+        this.setState({ result: average });
+        this.setState({ input: "" });
+      } else if (select === "mode") {
+        let count = {};
+        let highest = -Infinity;
+        arr.forEach((el) => {
+          if (count[el] > highest) {
+            count[el]++;
+            highest = Number(el);
+          } else {
+            count[el] = 1;
+          }
+        });
+        this.setState({ result: highest });
+        this.setState({ input: "" });
+      }
+    } else {
+      this.setState({ result: "Invalid input."});
+    }
   };
   render() {
-      const {input, select} = this.state
+    const { input, select, result } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
-        <input name="input" value={input} onChange={this.handleChange}/>
+        <input name="input" value={input} onChange={this.handleChange} />
         <select name="select" value={select} onChange={this.handleChange}>
-            <option></option>
-            <option>sum</option>
-            <option>average</option>
-            <option>mode</option>
+          <option></option>
+          <option>sum</option>
+          <option>average</option>
+          <option>mode</option>
         </select>
         <button>Calculate</button>
-        <h2></h2>
+        <h2>{result}</h2>
       </form>
     );
   }
