9e388e53ab731cc64ab90283d5190d6d199f43b8
Coreen Cooper <coreencooper@pursuit.org>
2021-05-03 17:49:25 -0400

    add calculateAverage

--- src/Components/Calculate.js
+++ src/Components/Calculate.js
@@ -4,29 +4,43 @@ class Calculate extends React.Component {
   state = {
     input: "",
     mathDefault: "sum",
-    result: null
+    result: null,
   };
-  
-  handleCalculation = () => {
-    const {mathDefault, input} = this.state
+
+  calculateSum = () => {
+    const { input } = this.state;
     const newArr = input.split(",").map(Number);
-    if(mathDefault === 'sum'){
-      this.setState({result: newArr.reduce((acc, el) => acc + el, 0)});
-    } else if (mathDefault === 'average') {
-      console.log('average');
-    } else if (mathDefault === 'mode') {
-      console.log('mode');
-    }}
+    return newArr.reduce((acc, el) => acc + el, 0)
+  };
+
+  calculateAverage = (sum) => {
+    const { input } = this.state;
+    const newArr = input.split(",").map(Number);
+      return this.setState({ result: sum / newArr.length})
+  }
+
+  handleCalculation = () => {
+    const { mathDefault } = this.state;
+    // const newArr = input.split(",").map(Number);
+    let sum = this.calculateSum();
+    if (mathDefault === "sum") {
+      this.setState({ result: sum })
+    } else if (mathDefault === "average") {
+      this.calculateAverage(sum)
+    } else if (mathDefault === "mode") {
+      // solve for mode
+      this.setState({ result: "hi"})
+    }
+  };
 
   handleInputCalculate = (e) => {
     e.preventDefault();
     this.handleCalculation();
-    console.log(this.state);
   };
 
   handleInputChange = (e) => {
     this.setState({
-      input: e.target.value
+      input: e.target.value,
     });
   };
 
@@ -38,9 +52,7 @@ class Calculate extends React.Component {
     const { input, result, mathDefault } = this.state;
     return (
       <>
-        <form
-          onSubmit={this.handleInputCalculate}
-        >
+        <form onSubmit={this.handleInputCalculate}>
           <label>
             Enter each number in the array, separated by a ','
             <input onChange={this.handleInputChange} value={input} />
@@ -59,6 +71,3 @@ class Calculate extends React.Component {
 }
 
 export default Calculate;
-
-
-
