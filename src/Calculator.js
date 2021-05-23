ab6e16c02beb32166ce79439be398803d125f737
Kelvin Zheng <kzheng000@citymail.cuny.edu>
2021-05-23 00:49:57 -0400

    had issue on line 150

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+/* import React, { Component } from 'react'
 
 export class Calculator extends Component {
     constructor(){
@@ -95,5 +95,91 @@ export class Calculator extends Component {
     }
 }
 
-export default Calculator
+export default Calculator */
 
+
+import React, {useState} from 'react'
+
+export default function Calculator() {
+    const [inputNumbers, setInputNumbers] = useState("")
+    const [selectedOperation, setSelectedOperation] = useState("")
+    const [result, setResult] = useState("")
+    const [numberSplit, setNumberSplit] = useState([])
+    
+    const handleOperation = (event) => {
+        setSelectedOperation(event.target.value);
+    }
+    const handleChange = (event) => {
+        setInputNumbers(event.target.value);
+    }
+    const sum = () => {
+        let total = 0 
+        console.log(numberSplit)
+        numberSplit.forEach(element => total += element)
+        console.log(total)
+        setResult(total)
+        
+        return result
+    }
+    const average = () => {
+        let sum = sum()
+        setResult(sum / numberSplit.length)  
+    }
+    const mode = () => {
+        const object = {}
+        let highest = 0 
+        let highestKey = -Infinity
+
+        numberSplit.forEach(num => {
+            object[num] = (object[num] || 0) + 1
+        })
+
+        for (let key in object) {
+            const selectedOperation = object[key]
+            if (selectedOperation > highest){
+                highest = selectedOperation
+                highestKey = key
+            }
+        }
+
+        setResult(highestKey)
+
+    }
+    const checker = () => {
+        setNumberSplit(inputNumbers.split(",").map((element) => Number(element)))
+        //number split does not log out anything
+        console.log(numberSplit)
+        return numberSplit.every((character) => {
+        return ! Number.isNaN(character)
+        }) 
+    }
+    const handleCalculate = (event) => {
+        event.preventDefault()
+
+        if (selectedOperation === "sum" && checker()){
+            sum()
+        } else if (selectedOperation === "average" && checker()){
+            average()
+        } else if (selectedOperation === "mode" && checker() ){
+            mode()
+        } else {
+            setResult("Invalid input.")
+        } 
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleCalculate}>
+                <input type='text' name='inputNumbers' id='inputNumbers' selectedOperation={inputNumbers} onChange={handleChange} />
+                <select value={selectedOperation} onChange={handleOperation}>
+                    <option selectedOperation='none'></option>
+                    <option selectedOperation='sum'>sum</option>
+                    <option selectedOperation='average'>average</option>
+                    <option selectedOperation='mode'>mode</option>
+                </select>
+                <button type='submit'>Calculate</button>
+            </form>
+            <h2>{result}</h2>
+        </div>
+    )
+}
