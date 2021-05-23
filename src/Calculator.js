ce1f8afcac40236f3e07368840eceeee91324b6c
Kelvin Zheng <kzheng000@citymail.cuny.edu>
2021-05-22 22:43:54 -0400

    passed the test

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-/* import React, { Component } from 'react'
+import React, { Component } from 'react'
 
 export class Calculator extends Component {
     constructor(){
@@ -14,13 +14,13 @@ export class Calculator extends Component {
 
     handleOperation = (event) => {
         this.setState({
-            selectedOperation: event.target.selectedOperation
+            selectedOperation: event.target.value
         })
     }
     
     handleChange = (event) => {
         this.setState({
-            inputNumbers: event.target.selectedOperation
+            inputNumbers: event.target.value
         })
     }
     sum = () => {
@@ -71,7 +71,7 @@ export class Calculator extends Component {
         } else if (this.state.selectedOperation === "mode" && this.checker() ){
             this.mode()
         } else {
-            this.setState({result:"Invalid input"})
+            this.setState({result: "Invalid input."})
         } 
     }
 
@@ -81,7 +81,7 @@ export class Calculator extends Component {
         <div>
             <form onSubmit={this.handleCalculate}>
                 <input type='text' name='inputNumbers' id='inputNumbers' selectedOperation={inputNumbers} onChange={this.handleChange} />
-                <select selectedOperation={selectedOperation} onChange={this.handleOperation}>
+                <select value={selectedOperation} onChange={this.handleOperation}>
                     <option selectedOperation='none'></option>
                     <option selectedOperation='sum'>sum</option>
                     <option selectedOperation='average'>average</option>
@@ -96,116 +96,4 @@ export class Calculator extends Component {
 }
 
 export default Calculator
-*/
-import React, { Component } from 'react'
 
-export class Calculator extends Component {
-    constructor(){
-        super()
-        this.state = {
-            inputNumber: "",
-            selectedOperation: "",
-            result:""
-        }
-        this.result = ""
-        this.newinputNumber = []    
-
-    }
-
-    handleinputNumberChange = (e) => {
-        this.setState({inputNumber: e.target.selectedOperation})       
-    }
-
-
-    changeselectedOperation = (e) => {
-        this.setState({selectedOperation: e.target.selectedOperation})
-    }
-
-    checker = () => {
-        this.setState {
-            newinputNumber = this.state.inputNumber.split(",").map((el) => Number(el))
-
-        return this.newinputNumber.every((char) => {
-        return ! Number.isNaN(char)
-        }) 
-   }
-
-    handleCalculate = (e) => {
-        e.preventDefault()
-
-        if (this.state.selectedOperation === "sum" && this.checker() ){
-            this.sum()
-        } else if (this.state.selectedOperation === "average" && this.checker() ){
-            this.average()
-        } else if (this.state.selectedOperation === "mode" && this.checker() ){
-            this.mode()
-        } else {
-            this.setState({result:"Invalid input."})
-        } 
-    }
-
-    sum = () => {
-        let total = 0 
-        this.newinputNumber.forEach(el => total += el)
-        this.setState({result: total})
-        return total
-    }
-
-    average = () => {
-        let sum = this.sum()
-        this.setState({result: (sum / this.newinputNumber.length) })  
-    }
-
-    mode = () => {
-        const obj = {}
-        let highest = 0 
-        let highestKey = -Infinity
-
-        this.newinputNumber.forEach(num => {
-            obj[num] = (obj[num] || 0) + 1
-        })
-
-        for (let key in obj) {
-            const selectedOperation = obj[key]
-            if (selectedOperation > highest){
-                highest = selectedOperation
-                highestKey = key
-            }
-        }
-
-        this.setState({result:highestKey})
-
-    }
-
-    render() {
-        const {inputNumber,selectedOperation} = this.state
-        return (
-            <> 
-            <form onSubmit={this.handleCalculate}>
-                <input 
-                    type="text"
-                    name="inputNumber"
-                    id="inputNumber"
-                    onChange={this.handleinputNumberChange}
-                    selectedOperation = {inputNumber}
-                /> 
-                <br></br>
-                <select onChange={this.changeselectedOperation} selectedOperation={selectedOperation}>
-                    <option> </option>
-                    <option selectedOperation="sum">sum</option>
-                    <option selectedOperation="average">average</option>
-                    <option selectedOperation="mode">mode</option>
-                </select>
-                <br></br>
-                <button>Calculate</button>
-
-            </form>
-
-            <p> {this.state.result}</p>
-            </>
-
-        )
-    }
-}
-
-export default Calculator
